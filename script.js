0eadac0925c724c412992e6dc169ee3edb691862
rodilloferre <cesar.jarra@gmail.com>
2025-06-06 20:32:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function saludar() {
   alert("Agregado al carrito");
 }
+
+function cambiarCantidad(valor) {
+  const input = document.getElementById("cantidad");
+  let cantidad = parseInt(input.value);
+
+  cantidad += valor;
+
+  // Evitar que baje de 1
+  if (cantidad < 1) cantidad = 1;
+
+  input.value = cantidad;
+}
